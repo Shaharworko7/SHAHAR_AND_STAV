6c5a58253377dc51f874e62af85abe958a4a815b
idk <stavivgi0@gmail.com>
2024-09-10 12:38:41 +0300

    /

--- try.py
+++ try.py
@@ -1,19 +1,21 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import pygame
 
 # stav try:
+pygame.init()
+screen = pygame.display.set_mode((1400, 800))
+pygame.display.set_caption("womp")
+clock = pygame.time.Clock()
+running = True
+color = (138, 193, 230)
+
+while running:
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    key = pygame.key.get_pressed()
+    if pygame.key.get_pressed() == :
+
+        pygame.draw.circle(screen, (230, 138, 177), (700, 400), 200, 25)
+    pygame.draw.rect(screen, (92, 189, 70), (100, 200, 100, 200))
+    pygame.display.update()
