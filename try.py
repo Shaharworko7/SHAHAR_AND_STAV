1db091892df284c52ab10a86d75e741fb9448810
idk <stavivgi0@gmail.com>
2024-09-10 15:41:51 +0300

    /

--- try.py
+++ try.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # stav try:
 pygame.init()
@@ -6,27 +7,28 @@ screen = pygame.display.set_mode((750,375))
 pygame.display.set_caption("womp")
 clock = pygame.time.Clock()
 running = True
-color = (138, 193, 230)
+color = (95, 158, 47)
 x = 1
 y = 1
 flag = pygame.image.load("C:\\Users\\jbt\\Desktop\\pixil-frame-0.png")
-soilder = pygame.image.load("C:\\Users\\jbt\\Desktop\\pixil-frame-e.png")
+soldier = pygame.image.load("C:\\Users\\jbt\\Desktop\\pixil-frame-e.png")
+grass = pygame.image.load()
 
 
 while running:
     screen.fill(color)
     screen.blit(flag, (670, 280))
-    screen.blit(soilder, (x,y))
+    screen.blit(soldier, (x,y))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     key = pygame.key.get_pressed()
-    if key[pygame.K_RIGHT] and x < 685:
+    if key[pygame.K_RIGHT] and x < 680:
         x += 1
     if key[pygame.K_LEFT] and x > 1:
         x -= 1
     if key[pygame.K_UP] and y > 1:
         y -= 1
-    if key[pygame.K_DOWN] and y < 275:
+    if key[pygame.K_DOWN] and y < 295:
         y += 1
     pygame.display.update()
