f7d39665925c9fb226653fe9bf793cf553b95dce
idk <stavivgi0@gmail.com>
2024-09-11 10:03:07 +0300

    try 1 matrix

--- setting.py
+++ setting.py
@@ -1,4 +1,14 @@
+import pygame
+
 WIDTH = 800
-HEIGHT = 600
+HEIGHT = 400
 TITLE = "womp"
 COLOR_BG = (95, 158, 47)
+TILE_SIZE = 16
+GRID_WIDTH = WIDTH / TILE_SIZE
+GRID_HEIGHT = HEIGHT / TILE_SIZE
+
+# stav graphics
+FLAG = pygame.image.load("C:\\Users\\jbt\\Desktop\\pygame_grapics\\pixil-frame-0.png")
+SOLDIER = pygame.image.load("C:\\Users\\jbt\\Desktop\\pygame_grapics\\pixil-frame-e.png")
+GRASS = pygame.image.load("C:\\Users\\jbt\\Desktop\\pygame_grapics\\pixil-frame-green.png")
--- try.py
+++ try.py
@@ -12,30 +12,31 @@ clock = pygame.time.Clock()
 running = True
 x = 1
 y = 1
-flag = pygame.image.load("C:\\Users\\jbt\\Desktop\\pygame_grapics\\pixil-frame-0.png")
-soldier = pygame.image.load("C:\\Users\\jbt\\Desktop\\pygame_grapics\\pixil-frame-e.png")
-grass = pygame.image.load("C:\\Users\\jbt\\Desktop\\pygame_grapics\\pixil-frame-green.png")
 
 while running:
+    def draw_grid(self):
+        for i in range(0, WIDTH, TILE_SIZE):
+            pygame.draw.line(self.screen, (0,0,0), (i, 0), (i, HEIGHT))
+        for j in range(0, HEIGHT, TILE_SIZE):
+            pygame.draw.line(self.screen, (0,0,0), (0, j), (WIDTH, j))
     # for i in range(20):
     #     location_x = random.randint(0, 50)
     #     location_y = random.randint(0, 25)
-    #     matrix_game[location_y][location_x] = grass
+    #     matrix_game[location_y][location_x] = GRASS
     #     screen.blit(matrix_game[location_y][location_x], 1,1)
-    screen.fill(COLOR_BG)
-
-    screen.blit(flag, (670, 280))
-    screen.blit(soldier, (x, y))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    key = pygame.key.get_pressed()
-    if key[pygame.K_RIGHT] and x < 680:
-        x += 1
-    if key[pygame.K_LEFT] and x > 1:
-        x -= 1
-    if key[pygame.K_UP] and y > 1:
-        y -= 1
-    if key[pygame.K_DOWN] and y < 295:
-        y += 1
-    pygame.display.update()
+    # screen.fill(COLOR_BG)
+    # screen.blit(FLAG, (WIDTH - 80, HEIGHT - 95))
+    # screen.blit(SOLDIER, (x, y))
+    # for event in pygame.event.get():
+    #     if event.type == pygame.QUIT:
+    #         running = False
+    # key = pygame.key.get_pressed()
+    # if key[pygame.K_RIGHT] and x < WIDTH - 70:
+    #     x += 1
+    # if key[pygame.K_LEFT] and x > 1:
+    #     x -= 1
+    # if key[pygame.K_UP] and y > 1:
+    #     y -= 1
+    # if key[pygame.K_DOWN] and y < HEIGHT - 80:
+    #     y += 1
+    # pygame.display.update()
